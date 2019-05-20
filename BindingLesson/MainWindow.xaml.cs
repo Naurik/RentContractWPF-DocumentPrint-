c3adb5d56d3140fcc803ef189ed8057b996b1a41
Машимов Наурызбай Кенесариевич <@corp.itstep>
2019-05-20 21:02:47 +0600

    DocumentPrint

---
+++ BindingLesson/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace BindingLesson
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ClickToPrint(object sender, RoutedEventArgs e)
+        {
+            PrintDialog print = new PrintDialog();
+            if (print.ShowDialog() == true)
+            {
+                print.PrintVisual(richTextBox1, "Договор найма жилого помещения");
+            }
+        }
+    }
+}
